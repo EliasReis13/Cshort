4ad00810d6ae8fe610cfe12a76ebbd4bf6811c20
EliasReis <eliasreis1704@gmail.com>
2025-05-22 20:55:35 -0300

    Add keywords.h header file with keyword functions

---
+++ include/keywords.h
@@ -0,0 +1,7 @@
+#ifndef KEYWORDS_H
+#define KEYWORDS_H
+
+int is_keyword(const char *lexeme);  // Retorna 1 se for palavra-chave, senão 0
+void init_keywords();                // Inicializa tabela de palavras-chave
+
+#endif
