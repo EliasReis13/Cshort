fe5ffa190c162f2dfe8c65018d2e152472bcbd27
EliasReis <eliasreis1704@gmail.com>
2025-06-04 22:16:34 -0300

    Remove unused files and clean up project structure

--- include/keywords.h
+++
@@ -1,8 +0,0 @@
-#ifndef KEYWORDS_H
-#define KEYWORDS_H
-
-int is_keyword(const char *lexeme);  // Verifica se é palavra-chave
-void init_keywords();                // Inicializa a tabela
-void free_keywords();                // Libera memória da tabela
-
-#endif
