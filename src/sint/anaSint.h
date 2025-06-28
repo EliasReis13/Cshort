843c0a161cb11378f62bb4d7039c9eda813474d1
EliasReis <eliasreis1704@gmail.com>
2025-06-28 17:00:02 -0300

    Refactor: Organize comments and structure in anaSint.h for clarity

--- src/sint/anaSint.h
+++ src/sint/anaSint.h
@@ -2,19 +2,26 @@
 #define ANASINT_H
 
 #include <stdio.h>
-#include "anaLex.h" // Analisador léxico do Cshort
+#include "anaLex.h" // Inclui as definições do analisador léxico (TOKEN, etc.)
 
-extern FILE *fd;
-extern TOKEN t;
-extern char TABS[200];
+// --- Variáveis Globais Externas ---
 
+extern FILE *fd;        // Ponteiro para o arquivo de código-fonte que está sendo analisado.
+extern TOKEN t;         // Armazena o token atual que está sendo processado pelo parser.
+extern char TABS[200];  // String usada para controlar a indentação na impressão da árvore sintática.
+
+// --- Protótipos das Funções do Parser ---
+
+//Função principal e ponto de entrada do analisador sintático.
 void Prog();
 
+// --- Funções de Análise de Declarações ---
 void Decl();
-void DeclVar();
+void DeclVar(); 
 int Tipo();
-void Tipo_param();
+void Tipo_param(); 
 
+// --- Funções de Análise de Estruturas e Comandos ---
 void Func();
 
 void Atrib();
@@ -24,8 +31,11 @@ void Cmd_while();
 void Cmd_for();
 void Cmd_return();
 
+
+// --- Funções de Análise de Expressões ---
 void Termo();
 void Fator();
 void Expres();
 void Expres_simp();
+
 #endif
