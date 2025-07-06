560aa3c7b4142b91a6ef78e2097e6e2505968cb1
EliasReis <eliasreis1704@gmail.com>
2025-07-06 13:28:42 -0300

    Refactor parser header: add lookahead support, update declaration enum, and enhance function prototypes

--- src/sint/anaSint.h
+++ src/sint/anaSint.h
@@ -2,12 +2,22 @@
 #define ANASINT_H
 
 #include <stdio.h>
-#include <stdbool.h> // Adicionado para bool
+#include <stdbool.h> 
 #include "../lex/anaLex.h"
+
+// --- Enum para o sinalizador de declaração ---
+// Adicionado conforme especificado no PDF para a estratégia de análise fatorada.
+typedef enum { 
+    NO_DECL, 
+    DECL_VAR, 
+    DECL_PROT, 
+    DECL_FUNC 
+} DECL_SINALIZADOR;
 
 // --- Variáveis Globais Externas ---
 extern FILE *fd;
 extern TOKEN t;
+extern TOKEN tLookahead; // Adicionado para suportar a análise com lookahead
 extern char TABS[200];
 extern int contLinha;
 extern bool modoPanico;
@@ -15,17 +25,16 @@ extern bool houveErroSintatico;
 
 // --- Protótipos das Funções do Parser ---
 
-// Ponto de entrada e rotinas de alto nível
+// Ponto de entrada e rotinas de alto nível (estrutura do PDF)
 void Prog();
-void Decl_ou_Func();
+DECL_SINALIZADOR Decl(); // Modificada para retornar o sinalizador [cite: 39]
+void corpo_func();       // Nova função para tratar o corpo de uma função [cite: 32]
 int  Tipo();
 
-// Análise de corpos de função e declaração
-void Func_body();
-void Decl_var_body();
+// Análise de parâmetros
 void Tipos_param();
 
-// Análise de Comandos
+// Análise de Comandos (permanecem os mesmos)
 void Cmd();
 void Cmd_if();
 void Cmd_while();
@@ -35,7 +44,7 @@ void Cmd_bloco();
 void Cmd_break();
 void Cmd_continue();
 
-// Análise de Expressões (com tratamento de precedência)
+// Análise de Expressões (permanecem as mesmas)
 void Expr();
 void Expr_atrib();
 void Expr_ou();
