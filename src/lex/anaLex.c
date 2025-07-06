e4872c99cebde3a8fcbb301bb218767cf49bc3b5
EliasReis <eliasreis1704@gmail.com>
2025-07-06 13:28:32 -0300

    Add support for 'void' as a reserved keyword in the lexer

--- src/lex/anaLex.c
+++ src/lex/anaLex.c
@@ -39,6 +39,8 @@ static int reconhecerPalavraReservada(char lexema[]) {
     if (strcmp(lexema, "return") == 0)   return PR_RETURN;
     if (strcmp(lexema, "break") == 0)    return PR_BREAK;
     if (strcmp(lexema, "continue") == 0) return PR_CONTINUE;
+    if (strcmp(lexema, "void") == 0)     return PR_VOID;
+    
     return 0;
 }
 
